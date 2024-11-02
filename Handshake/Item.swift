3eed8b1add49eb2eca34a58de3b8d43fdfe46d54
steadman1 <steadman@vt.edu>
2024-11-02 13:02:09 -0400

    Initial Commit

---
+++ Handshake/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Handshake
+//
+//  Created by Spencer Steadman on 11/2/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HandshakeTests/HandshakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HandshakeTests.swift
+//  HandshakeTests
+//
+//  Created by Spencer Steadman on 11/2/24.
+//
+
+import Testing
+@testable import Handshake
+
+struct HandshakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
